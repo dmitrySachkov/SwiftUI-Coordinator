b82ce6b0a97cffe949fcdf39af8c5d72a5daa52d
domont <d.a.sachkov@gmail.com>
2024-11-23 11:21:50 +0300

    Initial Commit

---
+++ Coordinator SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Coordinator SwiftUI
+//
+//  Created by Dmitry Sachkov on 23.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Coordinator SwiftUI/Coordinator_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coordinator_SwiftUIApp.swift
+//  Coordinator SwiftUI
+//
+//  Created by Dmitry Sachkov on 23.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Coordinator_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
